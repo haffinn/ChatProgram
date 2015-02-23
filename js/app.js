3f8fadf297e44765a2f61eb1a89adfc69cc6000c
kotkarl <kotkarl@gmail.com>
2015-02-23 00:12:34 +0000

    bower_components done

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ ChatClient.controller('RoomController', function ($scope, $location, $rootScope,
 	$scope.currentRoom = $routeParams.room;
 	$scope.currentUser = $routeParams.user;
 	$scope.currentUsers = [];
-	$scope.errorMessage = ''
+	$scope.errorMessage = '';
 
 	socket.on('updateusers', function (roomName, users, ops) {
 		// TODO: Check if the roomName equals the current room !
