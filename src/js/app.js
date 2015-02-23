b42c82f12d48a1b9c74cd2fc80e561ca18575e78
kotkarl <kotkarl@gmail.com>
2015-02-23 16:35:26 +0000

    fixed paths in rooms

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,22 @@
 var ChatClient = angular.module('ChatClient', ['ngRoute']);
-
+ 
 ChatClient.config(
 	function ($routeProvider) {
 		$routeProvider
-			.when('/login', { templateUrl: 'Views/login.html', controller: 'LoginController' })
-			.when('/rooms/:user/', { templateUrl: 'Views/rooms.html', controller: 'RoomsController' })
-			.when('/room/:user/:room/', { templateUrl: 'Views/room.html', controller: 'RoomController' })
+			.when('/login', { templateUrl: 'src/Views/login.html', controller: 'LoginController' })
+			.when('/rooms/:user/', { templateUrl: 'src/Views/rooms.html', controller: 'RoomsController' })
+			.when('/room/:user/:room/', { templateUrl: 'src/Views/room.html', controller: 'RoomController' })
 			.otherwise({
 	  			redirectTo: '/login'
 			});
 	}
 );
-
+ 
 ChatClient.controller('LoginController', function ($scope, $location, $rootScope, $routeParams, socket) {
 	
 	$scope.errorMessage = '';
 	$scope.nickname = '';
-
+ 
 	$scope.login = function() {			
 		if ($scope.nickname === '') {
 			$scope.errorMessage = 'Please choose a nick-name before continuing!';
@@ -31,24 +31,30 @@ ChatClient.controller('LoginController', function ($scope, $location, $rootScope
 		}
 	};
 });
-
+ 
 ChatClient.controller('RoomsController', function ($scope, $location, $rootScope, $routeParams, socket) {
 	// TODO: Query chat server for active rooms
 	$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
 	$scope.currentUser = $routeParams.user;
+	$scope.roomname = '';
+ 
+    $scope.newRoom = function() {
+        $scope.rooms.push($scope.roomname);
+    };
+ 
 });
-
+ 
 ChatClient.controller('RoomController', function ($scope, $location, $rootScope, $routeParams, socket) {
 	$scope.currentRoom = $routeParams.room;
 	$scope.currentUser = $routeParams.user;
 	$scope.currentUsers = [];
-	$scope.errorMessage = ''
-
+	$scope.errorMessage = '';
+ 
 	socket.on('updateusers', function (roomName, users, ops) {
 		// TODO: Check if the roomName equals the current room !
 		$scope.currentUsers = users;
 	});		
-
+ 
 	socket.emit('joinroom', $scope.currentRoom, function (success, reason) {
 		if (!success)
 		{
