3f07c1f8840fdfa9e7e5dcbda98914003f784936
Haffi <hafthort12@ru.is>
2015-02-24 00:01:19 +0000

    changed name of index file for automatic redirection

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ ChatClient.config(
 			.when('/rooms/:user/', { templateUrl: 'src/Views/rooms.html', controller: 'RoomsController' })
 			.when('/room/:user/:room/', { templateUrl: 'src/Views/room.html', controller: 'RoomController' })
 			.otherwise({
-	  			redirectTo: '/login'
+	  			redirectTo: 'login'
 			});
 	}
 );
